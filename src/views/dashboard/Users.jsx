30856306aa1adfb6ce79945b598cad3909ef5586
Anjaly-NA <anjaly.sayonetech@gmail.com>
2021-01-18 16:14:30 +0530

    changes

--- src/views/dashboard/Users.jsx
+++ src/views/dashboard/Users.jsx
@@ -34,88 +34,6 @@ import { connect } from "react-redux";
 import Loader from "../../components/Loader";
 import DialogBox from "../../components/DialogBox";
 
-// const datas = [
-//   {
-//     id: uuid(),
-//     ref: "CDD1049",
-//     amount: 30.5,
-//     customer: {
-//       name: "Ekaterina Tankova",
-//     },
-//     createdAt: 1555016400000,
-//     status: "pending",
-//   },
-//   {
-//     id: uuid(),
-//     ref: "CDD1048",
-//     amount: 25.1,
-//     customer: {
-//       name: "Cao Yu",
-//     },
-//     createdAt: 1555016400000,
-//     status: "delivered",
-//   },
-//   {
-//     id: uuid(),
-//     ref: "CDD1047",
-//     amount: 10.99,
-//     customer: {
-//       name: "Alexa Richardson",
-//     },
-//     createdAt: 1554930000000,
-//     status: "refunded",
-//   },
-//   {
-//     id: uuid(),
-//     ref: "CDD1046",
-//     amount: 96.43,
-//     customer: {
-//       name: "Anje Keizer",
-//     },
-//     createdAt: 1554757200000,
-//     status: "pending",
-//   },
-//   {
-//     id: uuid(),
-//     ref: "CDD1045",
-//     amount: 32.54,
-//     customer: {
-//       name: "Clarke Gillebert",
-//     },
-//     createdAt: 1554670800000,
-//     status: "delivered",
-//   },
-//   {
-//     id: uuid(),
-//     ref: "CDD1044",
-//     amount: 16.76,
-//     customer: {
-//       name: "Adam Denisov",
-//     },
-//     createdAt: 1554670800000,
-//     status: "delivered",
-//   },
-//   {
-//     id: uuid(),
-//     ref: "CDD1044",
-//     amount: 16.76,
-//     customer: {
-//       name: "Adam Denisov",
-//     },
-//     createdAt: 1554670800000,
-//     status: "delivered",
-//   },
-//   {
-//     id: uuid(),
-//     ref: "CDD1044",
-//     amount: 16.76,
-//     customer: {
-//       name: "Adam Denisov",
-//     },
-//     createdAt: 1554670800000,
-//     status: "delivered",
-//   },
-// ];
 const useStyle = makeStyles(() => ({
   root: {},
   actions: {
@@ -133,6 +51,9 @@ const useStyle = makeStyles(() => ({
   table: {
     minWidth: 700,
   },
+  tablerow:{
+    cursor:'pointer'
+  }
 }));
 
 const User = (props, { className, ...rest }) => {
@@ -221,6 +142,7 @@ const User = (props, { className, ...rest }) => {
                         key={user.id}
                         tabIndex={-1}
                         onClick={(event) => handleRowClick(event, user.id)}
+                        className={classes.tablerow}
                       >
                         <TableCell>{user.id}</TableCell>
                         <TableCell>{user.first_name}</TableCell>
