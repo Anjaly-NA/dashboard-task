59ca0a35d9f745e45f86a19e415b6777d8994a6c
Anjaly-NA <anjaly.sayonetech@gmail.com>
2021-02-03 17:38:02 +0530

    covid data tabularized

--- src/views/covid/Covid.jsx
+++ src/views/covid/Covid.jsx
@@ -9,7 +9,13 @@ import {
   AccordionDetails,
   Typography,
   CardHeader,
-  Box,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Page from "../../components/Page";
@@ -25,9 +31,19 @@ const useStyles = makeStyles((theme) => ({
   content: {
     flexDirection: "column",
   },
-  list:{
-    color:theme.palette.primary.color3
-  }
+  row: {
+    backgroundColor: theme.palette.primary.color5,
+  },
+  table: {
+    minWidth: 650,
+  },
+  heading: {
+    color: theme.palette.primary.main,
+  },
+  tablebody: {
+    color: theme.palette.primary.color3,
+  },
+  activecases: { color: theme.palette.primary.color1 },
 }));
 
 const Covid = () => {
@@ -39,18 +55,17 @@ const Covid = () => {
       .then((response) => {
         setData(response.data);
         // const objectArray = Object.entries(response.data);
-        // setData(objectArray);
         // objectArray.map((item) => {
         //   console.log(item, "item");
         //   const objectArray2 = Object.entries(item[1].districtData);
         //   objectArray2.map((i) => {
-        //     console.log(i[0], "i");
+        //     console.log(i, "i");
         //   });
         // });
       });
   }, []);
   return (
-    <Page className={classes.root} title="Covids">
+    <Page className={classes.root} title="Covid Status">
       <Container maxWidth={false}>
         <Card>
           <CardHeader title="Covid status" />
@@ -68,14 +83,92 @@ const Covid = () => {
                     </Typography>
                   </AccordionSummary>
                   <AccordionDetails className={classes.content}>
-                    {Object.entries(states[1].districtData).map((districts) => (
+                    <TableContainer component={Paper}>
+                      <Table
+                        className={classes.table}
+                        aria-label="simple table"
+                      >
+                        <TableHead>
+                          <TableRow className={classes.row}>
+                            <TableCell className={classes.heading}>
+                              Districts
+                            </TableCell>
+                            <TableCell
+                              className={classes.heading}
+                              align="right"
+                            >
+                              Active
+                            </TableCell>
+                            <TableCell
+                              className={classes.heading}
+                              align="right"
+                            >
+                              Confirmed
+                            </TableCell>
+                            <TableCell
+                              className={classes.heading}
+                              align="right"
+                            >
+                              Deceased
+                            </TableCell>
+                            <TableCell
+                              className={classes.heading}
+                              align="right"
+                            >
+                              Recovered
+                            </TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {Object.entries(states[1].districtData).map(
+                            (districts) => (
+                              <TableRow key={districts[1]}>
+                                <TableCell
+                                  component="th"
+                                  scope="row"
+                                  className={classes.tablebody}
+                                >
+                                  {districts[0]}
+                                </TableCell>
+                                <TableCell
+                                  align="right"
+                                  className={classes.activecases}
+                                >
+                                  {districts[1].active}
+                                </TableCell>
+                                <TableCell
+                                  align="right"
+                                  className={classes.tablebody}
+                                >
+                                  {districts[1].confirmed}
+                                </TableCell>
+                                <TableCell
+                                  align="right"
+                                  className={classes.tablebody}
+                                >
+                                  {districts[1].deceased}
+                                </TableCell>
+                                <TableCell
+                                  align="right"
+                                  className={classes.tablebody}
+                                >
+                                  {districts[1].recovered}
+                                </TableCell>
+                              </TableRow>
+                            )
+                          )}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+
+                    {/* {Object.entries(states[1].districtData).map((districts) => (
                       <Box component="div">
                         <Typography variant="caption" className={classes.list}>
                           {districts[0]}
                         </Typography>
                         <br />
                       </Box>
-                    ))}
+                    ))} */}
                   </AccordionDetails>
                 </Accordion>
               ))}
